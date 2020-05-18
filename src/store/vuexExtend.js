411fb2d25bfdec512ec478367332098f98ea87d4
wangxiping@banggood.com <wangxiping@banggood.com>
2020-05-18 13:24:35 +0800

    修改注释

--- src/store/vuexExtend.js
+++ src/store/vuexExtend.js
@@ -5,8 +5,8 @@ export default {
     moduleVuex.map((module) => {
       let keysMutationsName = Object.keys(options.modules[module].mutations) // 获取每个module mutation方法名 集合 Array
       let keysActionsName = Object.keys(options.modules[module].actions) // 获取每个module mutation方法名  集合 Array
-      let keysStateGettersName = Object.keys(options.modules[module].getters)
-      let stateName = Object.keys(options.modules[module].state)
+      let keysStateGettersName = Object.keys(options.modules[module].getters) // 获取每个module getters方法名 集合 Array
+      let stateName = Object.keys(options.modules[module].state) // 获取每个module state 集合 Array
       let state = mapState(module, stateName)
       let stateGetters = mapGetters(module, keysStateGettersName)
       let actionsFun = mapActions(module, keysActionsName)
