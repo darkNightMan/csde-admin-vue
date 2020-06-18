8f1a3c5211d3fd8c657b4bcd3df11bf5ee1d64b8
wangxiping@banggood.com <wangxiping@banggood.com>
2020-06-18 10:52:39 +0800

    修改菜单样式

--- src/pages/layout/menu.vue
+++ src/pages/layout/menu.vue
@@ -1,6 +1,6 @@
 <template>
   <el-container :style="`height:100%;background:${theme.backGround};border-right:solid 1px #e6e6e6;`"  v-loading="!$menu()">
-      <div style="padding:0px;" :style="{'height': ($windowHeight() + 100) + 'px'}">
+      <el-main :style="{'height': ($windowHeight() + 100) + 'px', 'width': '100%', 'padding': 0}">
         <el-scrollbar class="page-component__nav" style="height:100%">
           <template v-for="(item, parentIndex) in $menu()"  >
             <el-menu style="border-right:none;" :key="parentIndex"  :default-active="$menuIndex()" >
@@ -22,7 +22,7 @@
             </el-menu>
           </template>
         </el-scrollbar>
-      </div>
+      </el-main>
   </el-container>
 </template>
 <script>
